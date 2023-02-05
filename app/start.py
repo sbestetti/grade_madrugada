25a59df89cc9d55bf98086ec797a1307b22195d1
Sergio Bestetti <sbestetti@gmail.com>
2023-02-05 07:42:39 -0300

    Linted

--- app/start.py
+++ app/start.py
@@ -1,11 +1,15 @@
-import requests
-import logging
+# Imports de sistema
 import sys
-import mysql.connector
 from datetime import date, datetime
 import os
+
+# Imports de ferramentas
+import requests
+import mysql.connector
+import logging
 import pandas
 
+# Tratando possível argumento de data de início
 if len(sys.argv) == 1:
     DATA_DE_INICIO = datetime.today()
 else:
@@ -17,11 +21,10 @@ DB_HOST = os.getenv('DB_HOST')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWD = os.getenv('DB_PASSWD')
 DB_NAME = os.getenv('DB_NAME')
-CHUNK_SIZE = 100000 # Quantidade de registros a serem inseridos em cada INSERT
+CHUNK_SIZE = 100000  # Quantidade de registros a serem inseridos em cada INSERT
 
 # Configs das requisições HTTP
 API_SECRET = os.getenv('API_SECRET')
-
 URL_REGISTROS = 'https://publica.cerc.inf.br/app/tio/transaction/arquivos/enviados?linesPerPage=2000&page=0'
 URL_ARQUIVO = 'https://publica.cerc.inf.br/app/tio/transaction/arquivos/urls/download/fileControlId?received=false'
 
@@ -53,19 +56,19 @@ PARTICIPANTES = [
 # Constantes de queries
 INSERT_QUERY = '''
     INSERT IGNORE INTO grade_madrugada (
-        cnpj, 
-        referencia_externa, 
-        guid, 
-        horario, 
-        codigo_erro, 
+        cnpj,
+        referencia_externa,
+        guid,
+        horario,
+        codigo_erro,
         desc_erro
-    ) 
+    )
     VALUES (
-        %(cnpj)s, 
-        %(referencia_externa)s, 
-        %(guid)s, 
-        %(horario)s, 
-        %(codigo_erro)s, 
+        %(cnpj)s,
+        %(referencia_externa)s,
+        %(guid)s,
+        %(horario)s,
+        %(codigo_erro)s,
         %(desc_erro)s
     )
     '''
@@ -93,25 +96,31 @@ except mysql.connector.DatabaseError as e:
     exit()
 logging.info('Conexão estabelecida')
 
+
 def get_tio_headers() -> dict:
     # Recebe um secret e retorna um header com autenticação preenchida
-    
+
     headers = {
         'Authorization': f'Basic {API_SECRET}'
     }
     form_data = {'grant_type': 'client_credentials'}
-    response = requests.post('https://cad-prd.cerc.inf.br/oauth/token', data=form_data, headers=headers)
+    response = requests.post(
+        'https://cad-prd.cerc.inf.br/oauth/token',
+        data=form_data,
+        headers=headers
+    )
     token = response.json()['access_token']
     header = {
-    'Authorization': f'Bearer {token}'
+        'Authorization': f'Bearer {token}'
     }
 
     return header
 
 
 def get_links_by_cnpj(cnpj: str) -> list:
-    #Recebe o CNPJ de um participante e retorna uma lista de todos os arquivos recebidos na data especificada
-    
+    # Recebe o CNPJ de um participante e retorna uma lista
+    # de todos os arquivos recebidos na data especificada
+
     payload = {
         'companyDocument': cnpj,
         'fileLayoutId': '73e4ad69-9aa0-43d6-9931-3ef108b0fd0c',
@@ -124,16 +133,15 @@ def get_links_by_cnpj(cnpj: str) -> list:
     try:
         response = requests.put(URL_REGISTROS, headers=header, json=payload)
         response.raise_for_status()
-    except requests.exceptions.HTTPError:        
+    except requests.exceptions.HTTPError:
         logging.critical(f'Erro HTTP {response.status_code} durante busca dos arquivos do participante {cnpj}. Os dados podem estar incompletos!')
         return
-        
-    
+
     data = response.json()
     arquivos_recebidos = list()
-    for arquivo in data['result']['content']:    
+    for arquivo in data['result']['content']:
         _ = {
-            'participante': str(arquivo['fileName'])[11:19], # Essa string contém parte do CNPJ do participante, extraído do nome do arquivo 
+            'participante': str(arquivo['fileName'])[11:19],
             'id': arquivo['fileControlId'],
             'nome': arquivo['fileName'],
             'tempo_de_processamento': arquivo['processingTime']
@@ -144,12 +152,12 @@ def get_links_by_cnpj(cnpj: str) -> list:
 
 
 def get_files_by_links(link: list) -> None:
-    #Recebe a lista de arquivos de um participante e salva todos os registros em um arquivo local único
-    
+    # Recebe a lista de arquivos de um participante e salva todos os registros em um arquivo local único
+
     participante = link['participante']
 
     header = get_tio_headers()
-    
+
     url_atual = URL_ARQUIVO.replace('fileControlId', link['id'])
     try:
         response = requests.get(url_atual, headers=header)
@@ -157,9 +165,9 @@ def get_files_by_links(link: list) -> None:
         logging.debug(f'Status da resposta: {response.status_code}')
     except requests.exceptions.HTTPError:
         message = f'Erro HTTP {response.status_code} durante busca dos arquivos do participante {participante}. Os dados podem estar incompletos!'
-        logging.warning(message)            
+        logging.warning(message)
         raise Exception(message)
-            
+
     data = response.json()
     url_do_arquivo = data['result']
     arquivo = requests.get(url_do_arquivo, stream=True)
@@ -170,31 +178,35 @@ def get_files_by_links(link: list) -> None:
 
 
 def parse_file(participante: str) -> int:
-    #Move os dados do arquivo recebido para o banco
+    # Move os dados do arquivo recebido para o banco
 
     logging.info(f'Lendo arquivos do participante {participante}')
 
     total_de_registros = 0
 
     with pandas.read_csv(
-    './tmp_file',
-    sep=';',
-    chunksize=CHUNK_SIZE,
-    names=[
-        'referencia_externa',
-        'guid',
-        'horario',
-        'codigo_erro',
-        'desc_erro'
-        ]
+        './tmp_file',
+        sep=';',
+        chunksize=CHUNK_SIZE,
+        names=[
+            'referencia_externa',
+            'guid',
+            'horario',
+            'codigo_erro',
+            'desc_erro'
+            ]
     ) as reader:
         for chunk in reader:
-            registros = list()    
+            registros = list()
             for line in chunk.index:
-                new_time = datetime.strptime(chunk['horario'][line], '%Y-%m-%dT%H:%M:%S.%fZ')
+                new_time = datetime.strptime(
+                    chunk['horario'][line], '%Y-%m-%dT%H:%M:%S.%fZ'
+                )
                 registro = {}
                 registro['cnpj'] = str(participante)
-                registro['referencia_externa'] = str(chunk['referencia_externa'][line])
+                registro['referencia_externa'] = str(
+                    chunk['referencia_externa'][line]
+                )
                 registro['guid'] = str(chunk['guid'][line])
                 registro['horario'] = new_time
                 if chunk['codigo_erro'][line] != 0:
@@ -204,7 +216,7 @@ def parse_file(participante: str) -> int:
                     registro['desc_erro'] = lista_erro[1]
                 else:
                     registro['codigo_erro'] = 0
-                    registro['desc_erro'] = None                
+                    registro['desc_erro'] = None
                 registros.append(registro)
 
             try:
@@ -228,19 +240,24 @@ for cnpj in PARTICIPANTES:
     logging.info(f'Processando arquivos do participante {cnpj}')
     links = get_links_by_cnpj(cnpj)
     qtd_arquivos = len(links)
-    logging.info(f'{qtd_arquivos} arquivos encontrados para o participante {cnpj}')
+    logging.info(
+        f'{qtd_arquivos} arquivos encontrados para o participante {cnpj}'
+    )
     counter = 1
     for link in links:
-        logging.info(f'Baixando arquivo {counter} de {qtd_arquivos} do participante {cnpj}')
+        logging.info(
+            f'Baixando arquivo {counter} de {qtd_arquivos} do participante {cnpj}'
+        )
         try:
             get_files_by_links(link)
-        except Exception as e:
-            print(f'Erro ao baixar o arquivo {counter} de {qtd_arquivos}. Pulando')
+        except Exception:
+            print(
+                f'Erro ao baixar o arquivo {counter} de {qtd_arquivos}. Pulando'
+            )
             counter += 1
             continue
-        logging.info(f'Download finalizado. Iniciando processamento')
+        logging.info('Download finalizado. Iniciando processamento')
         numero_de_registros = parse_file(cnpj)
         logging.info(f'{numero_de_registros} registro processados')
         counter += 1
 logging.info('Processo finalizado')
-            
