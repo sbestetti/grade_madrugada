c81508d007e9eea84cd9ed2ce461c322252e20d1
Sergio Bestetti <sbestetti@gmail.com>
2023-02-11 10:23:37 -0300

    Logger fix

--- app/log_manager.py
+++ app/log_manager.py
@@ -7,13 +7,10 @@ import logging
 # Internos
 import config as cfg
 
-
-def get_logger():
-    logging.basicConfig(
-        filename=cfg.log_config['file'],
-        encoding=cfg.log_config['encoding'],
-        format=cfg.log_config['format'],
-        level=cfg.log_config['level']
-    )
-    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-    return logging
+logging.basicConfig(
+    filename=cfg.log_config['file'],
+    encoding=cfg.log_config['encoding'],
+    format=cfg.log_config['format'],
+    level=cfg.log_config['level']
+)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
