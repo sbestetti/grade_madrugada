d400fcd6441992ce30fe2417bbc6c4116f72a692
Sergio Bestetti <sergio@bestetti.net>
2023-03-15 20:42:32 -0300

    Back to regular use

--- app/start.py
+++ app/start.py
@@ -83,10 +83,9 @@ def worker_save_file_to_db():
         process_jobs.task_done()
 
 logging.info('----------------------------Iniciando execucao----------------------------')
-#participantes = dao.get_participantes()
-participantes = ['04870413000188']
+participantes = dao.get_participantes()
 for participante in participantes:
-    link_jobs.put(participante)
+    link_jobs.put(participante[0])
 link_jobs.put(None)
 link_fetch_thread = threading.Thread(target=worker_get_link_by_cnpj, daemon=True)
 link_fetch_thread.start()
